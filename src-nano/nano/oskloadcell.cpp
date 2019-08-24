e5d99e715e711fa528332498adbe8528dc60c7cd
mepix <mepix@mepix-mbp>
2019-08-24 10:42:07 -0700

    Testing Loadcel Data

--- src-nano/nano/oskloadcell.cpp
+++ src-nano/nano/oskloadcell.cpp
@@ -3,6 +3,7 @@
 FILE* OSKloadcell::m_serPort;
 char  OSKloadcell::m_readBuffer[1024];
 int OSKloadcell::m_numBytesRead;
+std::string OSKloadcell::m_dataline="";
 
 OSKloadcell::OSKloadcell(std::string id, int freq) : OSKdevice(id, freq)
 {
@@ -25,7 +26,7 @@ OSKloadcell::OSKloadcell(std::string id, int freq) : OSKdevice(id, freq)
     }
 }
 
-OSKloadcell::~OSKloadcell(){};
+OSKloadcell::~OSKloadcell(){}
 
 void OSKloadcell::start(){
     m_thread = new std::thread(run,this);
@@ -41,7 +42,49 @@ bool OSKloadcell::run(OSKloadcell *me){
         {
             //TODO: Write a real communication protocol so we can implement some timing
             printf(m_readBuffer);
+            m_dataline = std::string(m_readBuffer);
         }
     }
     return 0;
+}
+
+double OSKloadcell::getWeight(){
+    procDataLine();
+    return m_weight;
+}
+
+
+bool OSKloadcell::procDataLine(){
+#if (DEBUG_DATALINE == true)
+    m_dataline = "0.059,-0.075,-0.943,-3.092,-4.321,-4.410,-3.974,-1.874,-0.586,-0.105,-1.932,1.94";
+#else
+    static std::string lastDataline = "";
+    if(m_dataline == ""){
+        return false;
+    } else if(m_dataline == lastDataline){
+        return false;
+    } else {
+        lastDataline = m_dataline;
+    }
+#endif
+
+    std::string temp = m_dataline;
+    std::string data = "";
+    for(int i = 0; i < DATA_LENGTH; ++i){
+        std::size_t pos = temp.find_first_of(',');
+        data = temp.substr(0,pos);
+        temp = temp.substr(pos+1);
+        m_data[i] = std::stod(data); //Convert string to double
+        std::cout << temp << "\nDATAPT: " << data << "\n";
+    }
+
+    if(DEBUG_DATALINE){
+        for(int i = 0; i < DATA_LENGTH; ++i){
+            std::cout << m_data[i] << ",";
+        }
+        std::cout << std::endl;
+    }
+
+
+    return true;
 }
--- src-nano/nano/oskloadcell.h
+++ src-nano/nano/oskloadcell.h
@@ -7,6 +7,9 @@
 
 #include "oskdevice.h"
 
+#define DEBUG_DATALINE true
+#define DATA_LENGTH 12
+
 class OSKdevice;
 
 class OSKloadcell : public OSKdevice
@@ -15,12 +18,21 @@ public:
     OSKloadcell(std::string id, int freq);
     ~OSKloadcell();
     static bool run(OSKloadcell *me);
+    double getWeight();
     void start();
 private:
+    bool procDataLine();
+
     //Serial Communication Variables
     static FILE *m_serPort;
     static char m_readBuffer[1024];
     static int m_numBytesRead;
+    static std::string m_dataline;
+
+    //Data Variables
+    double m_data[DATA_LENGTH]; //LAST POINT IS THE AVERAGE!
+    double m_weight;
+    double m_deltaWeight;
 
 };
 
